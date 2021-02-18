207ff2c5a01b54ae4ed7dd63075c6cfab705096d
charmockridge <charliemockridge@icloud.com>
2021-02-18 03:12:26 +0000

    updated code

--- main.py
+++ main.py
@@ -1,125 +1,144 @@
-import tkinter as Tk
-from tkinter import filedialog
-from tkinter import StringVar
+from tkinter import Tk, Label, Button, Entry, filedialog, messagebox
 import os
 import shutil
 
 
-root = Tk.Tk()
-root.title("File Extension Mover")
+class Window:
+    targetDir = ""
+    destineDir = ""
+    targetExt = ""
+    target = ""
+    destination = ""
+
+    def Reset(self):
+        self.targetDir = ""
+        self.destineDir = ""
+        self.targetExt = ""
+        self.target = ""
+        self.destination = ""
+
+    def SelectTargetDir(self):
+        self.targetDir = filedialog.askdirectory()
+        messagebox.showinfo(
+            title="Target Directory Selected",
+            message=f"You have selected { self.targetDir }"
+        )
+
+    def SelectDestineDir(self):
+        self.destineDir = filedialog.askdirectory()
+        messagebox.showinfo(
+            title="Destination Directory Selected",
+            message=f"You have selected { self.destineDir }"
+        )
+
+    def Script(self):
+        try:
+            self.targetExt = str(self.entTargetExt.get())
+
+            for path, root, files in os.walk(self.targetDir):
+                for f in files:
+                    if f.endswith(self.targetExt):
+                        self.target = f"{ self.targetDir }/{ f }"
+                        self.destination = f"{ self.destineDir }/{ f }"
+                        shutil.move(self.target, self.destination)
+
+            messagebox.showinfo(
+                title="Files Moved",
+                message=f"Your { self.targetExt } files in { self.targetDir }" +
+                        f" have been successfully moved to { self.destineDir }!"
+            )
+
+            self.Reset()
+        except:
+            messagebox.showerror(
+                title="Unable To Move Files",
+                message="An error has occured whilst trying to move your " +
+                        "files. Please re-select your target extension, " +
+                        "target and destination directory and try again!"
+            )
+
+            self.Reset()
+
+    def __init__(self, master):
+        self.master = master
+
+        master.title("File Extension Mover")
+
+        self.lblTitle = Label(
+            master,
+            text="FILE EXTENSION MOVER"
+        )
+        self.lblTitle.grid(
+            row=0,
+            column=0,
+            columnspan=2,
+            sticky="NESW",
+            pady=(0, 10),
+            padx=2.5
+        )
+
+        self.btnTargetDir = Button(
+            master,
+            text="Select Target Folder",
+            command=self.SelectTargetDir
+        )
+        self.btnTargetDir.grid(
+            row=1,
+            column=0,
+            sticky="NESW",
+            pady=(0, 5),
+            padx=2.5
+        )
+
+        self.btnDestineDir = Button(
+            master,
+            text="Select Destination Folder",
+            command=self.SelectDestineDir
+        )
+        self.btnDestineDir.grid(
+            row=2,
+            column=0,
+            sticky="NESW",
+            pady=(0, 5),
+            padx=2.5
+        )
+
+        self.lblTargetExt = Label(
+            master,
+            text="Target Extension:"
+        )
+        self.lblTargetExt.grid(
+            row=3,
+            column=0,
+            sticky="W",
+            padx=2.5
+        )
+
+        self.entTargetExt = Entry(
+            master
+        )
+        self.entTargetExt.grid(
+            row=4,
+            column=0,
+            sticky="NESW",
+            pady=(0, 5),
+            padx=2.5
+        )
+
+        self.btnRunScript = Button(
+            master,
+            text="Move Files",
+            command=self.Script
+        )
+        self.btnRunScript.grid(
+            row=5,
+            column=0,
+            sticky="NESW",
+            pady=(0, 2.5),
+            padx=2.5
+        )
 
 
-def submitExtension():
-    txt3.configure(text="Targeted Extension: " + str(ent1.get()))
-
-
-def targetFolder():
-    global targetDir
-    targetDir = filedialog.askdirectory()
-    txt4.configure(text="Targeted Folder: " + targetDir)
-
-
-def destinationFolder():
-    global destinationDir
-    destinationDir = filedialog.askdirectory()
-    txt5.configure(text="Destination Folder: " + destinationDir)
-
-
-def startScript():
-    for path, root, files in os.walk(targetDir):
-        for x in files:
-            if x.endswith(str(ent1.get())):
-                source = targetDir + "/" + x
-                destination = destinationDir + "/" + x
-                shutil.move(source, destination)
-
-
-txt1 = Tk.Label(
-    root,
-    text="FILE EXTENSION MOVER",
-    font=("arial", 32, "bold")
-)
-txt1.grid(row=0, column=2)
-
-nl1 = Tk.Label(
-    root,
-    text=" "
-)
-nl1.grid(row=1, column=2)
-
-txt2 = Tk.Label(
-    root,
-    text="Enter Target Extension:",
-)
-txt2.grid(row=2, column=0)
-
-ent1 = Tk.Entry(
-    root,
-)
-ent1.grid(row=2, column=2)
-
-btn1 = Tk.Button(
-    root,
-    text="Target Extension",
-    command=submitExtension,
-)
-btn1.grid(row=2, column=4)
-
-nl3 = Tk.Label(
-    root,
-    text=" "
-)
-nl3.grid(row=3, column=2)
-
-btn2 = Tk.Button(
-    root,
-    text="Choose Target Folder",
-    command=targetFolder
-)
-btn2.grid(row=4, column=1)
-
-btn3 = Tk.Button(
-    root,
-    text="Choose Destination Folder",
-    command=destinationFolder
-)
-btn3.grid(row=4, column=3)
-
-nl4 = Tk.Label(
-    root,
-    text=" "
-)
-nl4.grid(row=5, column=2)
-
-txt3 = Tk.Label(
-    root,
-    text="Target Extension: "
-)
-txt3.grid(row=6, column=2)
-
-txt4 = Tk.Label(
-    root,
-    text="Target Folder: "
-)
-txt4.grid(row=7, column=2)
-
-txt5 = Tk.Label(
-    root,
-    text="Destination Folder: "
-)
-txt5.grid(row=8, column=2)
-
-nl5 = Tk.Label(
-    root,
-    text=" "
-)
-nl5.grid(row=9, column=2)
-
-btn4 = Tk.Button(
-    root,
-    text="Move Files",
-    command=startScript
-)
-btn4.grid(row=10, column=2)
-
-Tk.mainloop()
+root = Tk()
+gui = Window(root)
+root.mainloop()
